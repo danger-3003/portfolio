81dca98eb7abe3e2d19fbd45b72160e4e3c19222
sumanth-narem <sumanth.narem@reverfin.ai>
2025-07-30 21:28:33 +0530

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function page() {
+  return (
+    <>
+      <div className='font-nunito bg-red-500'>page</div>
+      <div className='font-poppins'>page</div>
+    </>
+  )
+}
+
+export default page;
